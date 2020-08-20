bef76dcc6ca964e9968b4f57d3db2cacf1e5fe0d
Alan mc Gee <mcgee.al@gmail.com>
2020-08-20 12:09:12 +0100

    Created addPost method using axios to write to db

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -6,37 +6,40 @@
         <div class="col-md-6">
           <div class="form-group">
             <label>Post Title:</label>
-            <input type="text" class="form-control" v-model="post.title">
+            <input type="text" class="form-control" v-model="post.title" />
           </div>
         </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-              <label>Post Body:</label>
-              <textarea class="form-control" v-model="post.body" rows="5"></textarea>
-            </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Body:</label>
+            <textarea class="form-control" v-model="post.body" rows="5"></textarea>
           </div>
-        </div><br />
-        <div class="form-group">
-          <button class="btn btn-primary">Create</button>
         </div>
+      </div>
+      <br />
+      <div class="form-group">
+        <button class="btn btn-primary">Create</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-
-    export default {
-        data(){
-        return {
-          post:{}
-        }
+export default {
+  data() {
+    return {
+      post: {},
+    };
+  },
+  methods: {
+    addPost() {
+      let uri = "http://localhost:4000/posts/add";
+      this.axios.post(uri, this.post).then(() => {
+        this.$router.push({ name: "posts" });
+      });
     },
-    methods: {
-      addPost(){
-        console.log(this.post);
-      }
-    }
-  }
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// main.js
-
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
